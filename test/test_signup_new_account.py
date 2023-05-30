a1ac1d66974b0cadb6765301371f4a63a82ff794
Oleg Ivanov <olegivanov1989@gmail.com>
2023-05-30 17:44:24 +0300

    Improve test by adding asserts with SOAP protocol

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -13,8 +13,5 @@ def test_signup_new_account(app):
 
     app.james.ensure_user_exists(username, password)
     app.signup.add_new_user(username, email, password)
-    app.session.login(username, password)
 
-    assert app.session.is_logged_in_as(username)
-
-    app.session.logout()
+    assert app.soap.can_login(username, password)
